5c773621390ba6b145515981b9cf1f40f833f071
Otávio Fernandes <ofernandes@schubergphilis.com>
2019-04-13 03:39:45 +0200

    Adding Landscaper items to runtime config.

--- pkg/galaxy/config.go
+++ pkg/galaxy/config.go
@@ -1,6 +1,7 @@
 package galaxy
 
 import (
+	"os"
 	"strings"
 )
 
@@ -10,9 +11,31 @@ type Config struct {
 	DryRun        bool   // dry-run flag
 	LogLevel      string // log verboseness
 	Environments  string // target environment names, comma separated
+
+	*LandscaperConfig
 }
 
-// GetEnvironments as slice of strings.:w
+// LandscaperConfig runtime configuration related to Landscaper.
+type LandscaperConfig struct {
+	InCluster        bool   // inside a Kubernetes cluster
+	KubeConfig       string // path to alternative ~/.kube/config
+	KubeContext      string // kubernetes context
+	HelmHome         string // helm home folder
+	TillerNamespace  string // helm tiller kubernetes namespace
+	TillerPort       int    // helm tiller pod service port
+	TillerTimeout    int64  // helm tiller connection timeout (seconds)
+	OverrideFile     string // configuration override file
+	WaitForResources bool   // wait for resources flag
+	WaitTimeout      int64  // wait for resources timeout
+	DisabledStages   string // comma separated list of disabled stages
+}
+
+// GetDisabledStages return a slice of strings based on disabled stages.
+func (l *LandscaperConfig) GetDisabledStages() []string {
+	return strings.Split(l.DisabledStages, ",")
+}
+
+// GetEnvironments as slice of strings based on environments.
 func (c *Config) GetEnvironments() []string {
 	return strings.Split(c.Environments, ",")
 }
@@ -20,9 +43,15 @@ func (c *Config) GetEnvironments() []string {
 // NewConfig with default values.
 func NewConfig() *Config {
 	return &Config{
-		Environments:  "",
 		LogLevel:      "error",
 		DryRun:        false,
 		DotGalaxyPath: ".galaxy.yaml",
+		LandscaperConfig: &LandscaperConfig{
+			HelmHome:        os.ExpandEnv("${HOME}/.helm"),
+			TillerNamespace: "kube-system",
+			TillerPort:      44134,
+			TillerTimeout:   30,
+			WaitTimeout:     60,
+		},
 	}
 }
---
+++ pkg/galaxy/config_test.go
@@ -0,0 +1,37 @@
+package galaxy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var cfg *Config
+
+func TestConfigNew(t *testing.T) {
+	cfg = NewConfig()
+
+	t.Logf("Config: '%#v'", cfg)
+
+	assert.Equal(t, "error", cfg.LogLevel)
+	assert.Equal(t, int64(60), cfg.WaitTimeout)
+
+	// making sure sub-struct is present
+	assert.NotNil(t, cfg.LandscaperConfig)
+}
+
+func TestConfigGetDisabledStages(t *testing.T) {
+	cfg.DisabledStages = "one"
+	assert.Equal(t, []string{"one"}, cfg.GetDisabledStages())
+
+	cfg.DisabledStages = "one,two"
+	assert.Equal(t, []string{"one", "two"}, cfg.GetDisabledStages())
+}
+
+func TestConfigGetEnvironments(t *testing.T) {
+	cfg.Environments = "one"
+	assert.Equal(t, []string{"one"}, cfg.GetEnvironments())
+
+	cfg.Environments = "one,two"
+	assert.Equal(t, []string{"one", "two"}, cfg.GetEnvironments())
+}
